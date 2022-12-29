04fec555eab22dcf20335042b1b4433820851d67
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-12-29 08:33:26 +0200

    modal backdrop and esc keydown close

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,7 +10,26 @@
   refs.openModalBtnSecond.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  function onCloseModal(event) {
+    if (
+      event.target === event.currentTarget ||
+      event.target === refs.closeModalBtn ||
+      event.target.parentElement === refs.closeModalBtn ||
+      event.target.parentElement.parentElement === refs.closeModalBtn ||
+      event.code === 'Escape'
+    ) {
+      toggleModal();
+    }
+  }
+
   function toggleModal() {
+    if (document.body.classList.contains('modal-open')) {
+      refs.modal.removeEventListener('click', onCloseModal);
+      window.removeEventListener('keydown', onCloseModal);
+    } else {
+      refs.modal.addEventListener('click', onCloseModal);
+      window.addEventListener('keydown', onCloseModal);
+    }
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
